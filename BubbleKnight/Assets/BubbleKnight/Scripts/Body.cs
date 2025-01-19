91f48514711cfb989786c47a0fa57ec3649c28d3
Gianniscai <952919804@qq.com>
2025-01-19 12:31:02 +0800

    受击夹角判断

--- BubbleKnight/Assets/BubbleKnight/Scripts/Body.cs
+++ BubbleKnight/Assets/BubbleKnight/Scripts/Body.cs
@@ -17,9 +17,27 @@ public class Body : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer == 7 || collision.gameObject.layer == 10)
+        
+        if (collision.gameObject.layer == 7 || collision.gameObject.layer == 10)
         {
-            GameManager._instance.TakeDamage();
+            Vector3 pos = collision.transform.position;
+
+            Vector3 vec = pos - this.transform.position;
+
+            float dotProduct = Vector2.Dot(new Vector2(vec.x, vec.y).normalized, new Vector2(0, -1));
+            // 计算夹角（弧度）
+            float angleInRadians = Mathf.Acos(dotProduct);
+            // 转换为角度
+            float angleInDegrees = Mathf.Rad2Deg * angleInRadians;
+            // 判断夹角是否在 90 度以内
+            if (angleInDegrees < 90 && collision.gameObject.layer == 7)
+            {
+                // 不掉血
+            } else
+            {
+                GameManager._instance.TakeDamage();
+            }
+            
         }
     }
 }
